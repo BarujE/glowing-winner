e0559397d4de44039c9e82f6f6dd59fd7df1baf3
Baruj <Entsiwahbaruj@gmail.com>
2025-11-13 20:43:17 +0000

    Add files via upload

---
+++ from pytube import YouTube.py
@@ -0,0 +1,18 @@
+from pytubefix import YouTube
+
+urls =  input("url:")
+
+vid = YouTube(urls)
+
+video_download = vid.streams.get_highest_resolution()
+audio_download = vid.streams.get_audio_only()
+
+entry = YouTube(urls).title
+
+print(f"\nVideo found: {entry}\n")
+
+print(f"Downloading Video...")
+video_download.download(filename=f"{entry}.mp4")
+
+print("audio Complete!")
+
