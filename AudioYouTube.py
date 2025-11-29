679bf732893ca96f7b81c46075de5c723ad1e35c
Baruj <Entsiwahbaruj@gmail.com>
2025-11-29 21:26:37 +0000

    audio format selection for download
    Added user input for selecting audio format and validation.

--- AudioYouTube.py
+++ AudioYouTube.py
@@ -1,5 +1,6 @@
 from pytubefix import YouTube
 
+
 urls =  input("url:")
 
 vid = YouTube(urls)
@@ -8,14 +9,27 @@ video_download = vid.streams.get_highest_resolution()
 audio_download = vid.streams.get_audio_only()
 
 entry = YouTube(urls).title
+pick = input("pick audio Format mp4, mp3, wav,:")
+if pick == "mp4" or pick == "mp3" or pick == "wav":
+    print(f"\audio found: {entry}\n")
 
-print(f"\nVideo found: {entry}\n")
 
-print(f"Downloading Video...")
-video_download.download(filename=f"{entry}.mp4")
-video_download.download(filename=f"{entry}.mp3")
-video_download.download(filename=f"{entry}.wav")
-print("audio Complete!")
+    print(f"Downloading audio...")
+    filename = f"{entry}.{pick}"
+    audio_download.download(filename=filename)
+    
+    print(f"audio Complete")
+else:
+    print("Invalid Audio Format please select mp4, mp3, or Wav you fucking idiot")
+
+    exit()
+
+print("thank you for using The Youtube audio Downloader Made by Baruj")
+
+
+
+
+
 
 
 
